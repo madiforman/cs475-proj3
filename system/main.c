0b303f5fa3a1d02b5cb3974727c2bbb4a8869a55
Ubuntu <ubuntu@ubuntu2004.linuxvmimages.local>
2022-04-15 17:38:52 -0400

    Fixed when a philo

--- system/main.c
+++ system/main.c
@@ -8,7 +8,10 @@
 
 // TODO - locks must be declared and initialized here
 mutex_t forks[N];
-mutex_t mutex;
+mutex_t mutex = FALSE;
+mutex_t left_fork;
+mutex_t right_fork;
+
 /**
  * Delay for a random amount of time
  * @param alpha delay factor
@@ -42,41 +45,44 @@ void think()
  */
 void philosopher(uint32 phil_id)
 {
-    uint32 right = forks[(phil_id + 1) % N]; // TODO - right fork
-    uint32 left = forks[phil_id];            // TODO - left fork
-
+    //find the index for right and left forks
+    uint32 right = (phil_id + 1) % N; // TODO - right fork
+    uint32 left = phil_id;            // TODO - left fork
+    // initiate a lock for both left and right fork
+    left_fork = forks[left];
+    right_fork = forks[right];
+    // srand with the seed being the phil_id
+    srand(phil_id);
     while (TRUE)
     {
-        srand(phil_id);
-        int r = rand() % 10;
-        mutex_t left_fork = forks[left];
-        mutex_t right_fork = forks[right];
-        if (r > 3)
-        { // think
-            // mutex_lock(&mutex);
+        if ((rand() % 10) >= 3)
+        { // think 70% chance
+            //lock up the print statement
+            mutex_lock(&mutex);
             kprintf("Philosopher %d thinking: zzzZZZzzz\n", phil_id);
+            mutex_unlock(&mutex);
+            //call think()
             think();
-            // mutex_unlock(&mutex);
         }
-        else if (r < 3)
-        {
+        else
+        {// eat 30% of the time
+            //eat when both the right fork and the left fork are available
             if (right_fork == FALSE && left_fork == FALSE)
             {
-                // mutex_lock(&mutex);
+                //lock up both forks
                 mutex_lock(&right_fork);
                 mutex_lock(&left_fork);
+                //lock up the print statement
+                mutex_lock(&mutex);
                 kprintf("Philosopher %d eating: nom nom nom\n", phil_id);
+                mutex_unlock(&mutex);
+                //call to eat()
                 eat();
-                mutex_unlock(&right_fork);
+                //unlock the forks
                 mutex_unlock(&left_fork);
-                // mutex_unlock(&mutex);
-            }
-            else
-            {
-                break;
+                mutex_unlock(&right_fork);
             }
         }
-        // mutex_unlock(&mutex);
     }
 }
 
